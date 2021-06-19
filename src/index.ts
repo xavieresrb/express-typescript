d1fcbac4ee9d3080baac385b4b2a1a477dd59b4a
xavieresrb <xavieres@gmail.com>
2021-06-19 12:12:26 +0200

    fix: add simple routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hi');
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
