5f65cba0fe9c6e12573a86a4d516a173824592d1
xavieresrb <xavieres@gmail.com>
2021-06-28 14:02:51 +0200

    feat: add decorators to routing

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
+import { AppRouter } from './AppRouter';
+
+import './controllers/LoginController';
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cookieSession({ keys: ['potato1234'] }));
-
-app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
